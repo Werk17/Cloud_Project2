6e80e56f8434275c662124c522b9a5129f1948cf
Charles Werk <charles.werk@umconnect.umt.edu>
2022-04-12 00:15:04 -0600

    Remove whitespace

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window._config = {
     cognito: {
-        userPoolId: ' us-west-2_C7GAR5Sx0', // e.g. us-east-2_uXboG5pAb
+        userPoolId: 'us-west-2_C7GAR5Sx0', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '54pc6anu2o1mm4l7mbej446kgq', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-west-2' // e.g. us-east-2
     },
